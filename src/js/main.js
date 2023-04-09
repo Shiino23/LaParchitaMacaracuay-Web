ef62b2195cd61258d7e82d4dc64a22ea6fc8f185
Shiino23 <ginoib23@gmail.com>
2023-04-09 19:18:32 -0300

    se reemplazaw fondo index.html y se agrega box shadow a logo

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,7 @@ const rainingLeafs = () =>{
     let leaf = document.createElement('i');
     let leaf2 = document.createElement('j')
     let x = innerWidth * Math.random();
-    let height = (Math.random() * 12) + 80;
-    let width = (Math.random() * 14) + 80;
+    let size = (Math.random() * 12) + 90;
     let z = Math.round(Math.random)* 0.1;
     let delay = Math.random() * 2;
     let anima = (Math.random() * 10) + 5;
@@ -16,8 +15,8 @@ const rainingLeafs = () =>{
     
     leaf.style.rotate = deg+'deg';
     leaf.style.left = x + 'px';
-    leaf.style.width = width + 'px';
-    leaf.style.height = height + 'px';
+    leaf.style.width = size + 'px';
+    leaf.style.height = size + 'px';
     leaf.style.zIndex = z;
     leaf.style.animationDelay = delay + 's';
     leaf.style.animationDuration = anima + 's';
