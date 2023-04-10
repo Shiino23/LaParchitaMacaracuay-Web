b25a66f3fc40b2cbfcff8745cb62acd0659262b5
Shiino23 <ginoib23@gmail.com>
2023-04-10 03:29:41 -0300

    Se trabajan medias query en index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,13 @@
 const body = document.querySelector('body')
 
+const mediaqueryList = window.matchMedia("(min-width: 920px)");
+
 
 const rainingLeafs = () =>{
     
     let leaf = document.createElement('i');
-    let leaf2 = document.createElement('j')
     let x = innerWidth * Math.random();
-    let size = (Math.random() * 12) + 90;
+    let size = (Math.random() * 10) + 90;
     let z = Math.round(Math.random)* 0.1;
     let delay = Math.random() * 2;
     let anima = (Math.random() * 10) + 5;
@@ -32,4 +33,7 @@ const rainingLeafs = () =>{
 
 };
 
-setInterval(rainingLeafs, 500);
+
+if (mediaqueryList.matches){
+    setInterval(rainingLeafs, 500);
+}
